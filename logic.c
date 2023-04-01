4afbd99758dda9572bf30d704337986dafc43255
Theo Daronat <theo.daronat@gmail.com>
2023-04-01 13:46:00 +0200

    add logic.c containing the first elements of the project

---
+++ logic.c
@@ -0,0 +1,22 @@
+#include "custom.h"
+
+const int ENABLE_END = 4*5;
+int enable = 1;
+uint32_t timer_count = 0;
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){
+    timer_count = 0;
+    enable = !enable;
+}
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
+    if (enable) {
+        GPIO_PinState state = HAL_GPIO_ReadPin(GPIOG, GPIO_PIN_13);
+        HAL_GPIO_WritePin(GPIOG, GPIO_PIN_13, !state);
+    } else {
+        timer_count++;
+        if (timer_count == ENABLE_END){
+            enable = 1;
+        }
+    }
+}
